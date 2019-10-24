2c623011627ad2c5ae6e4481ef30a099ecc934bb
KoryakimaYana <yana.kukushkina.1981@gmail.com>
2019-10-25 00:06:43 +0700

    -

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 #тут пишем команду
+import turtle
 def romb():
     import turtle
-    turtle.up()
-    turtle.goto(-400,-300)
     turtle.down()
     turtle.forward(150)
     turtle.left(60)
@@ -12,5 +11,32 @@ def romb():
     turtle.left(60)
     turtle.forward(150)
     turtle.mainloop()
+from turtle import *
+def square():
+    turtle.down()
+    for _ in range(4):
+        turtle.forward(150)
+        turtle.right(90)
+    turtle.up()
+    turtle.mainloop()
+def tria60():
+    down()
+    right(30)
+    for _ in range(3):
+        forward(150)
+        right(120)
+    up()
+    mainloop()
+def tria60big():
+    down()
+    right(30)
+    for _ in range(3):
+        forward(300)
+        right(120)
+    up()
+    mainloop()
+turtle.right(30)
+tria60big()
 # Grate
-# Ok !
+# Ok !turtle.up()
+#     turtle.goto(-400,-300)
